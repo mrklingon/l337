0a19987622cb94ce354e4e1b461442fdbc24d6e7
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2020-11-25 20:44:14 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     stellas[current].scrollImage(1, 200)
 })
+input.onGesture(Gesture.TiltLeft, function () {
+    stellas[current].scrollImage(-1, 200)
+})
 function Hyper () {
     L3.showImage(0)
     basic.pause(100)
@@ -43,6 +46,9 @@ input.onButtonPressed(Button.B, function () {
 input.onGesture(Gesture.Shake, function () {
     Hyper()
     current = randint(0, stellas.length - 1)
+    stellas[current].scrollImage(1, 200)
+})
+input.onGesture(Gesture.TiltRight, function () {
     stellas[current].scrollImage(1, 200)
 })
 let current = 0
@@ -111,9 +117,113 @@ let star6 = images.createBigImage(`
     . . . . . . # . . .
     . . . # . . . . . .
     `)
-stellas = [star1, star2, star3, star4, star5, star6]
-for (let value of stellas) {
-    value.scrollImage(1, 200)
+let star7 = images.createBigImage(`
+    . . . . . . . . . .
+    . . . # . . . . # .
+    . # . . . # . . . .
+    . . . # . . . . . .
+    . . . . . . . . . .
+    `)
+let star8 = images.createBigImage(`
+    . # # . . . . . . #
+    . . . . # . # . . #
+    . . . . . . . . . .
+    . . . # . . . # . .
+    . . . . . . # . . .
+    `)
+let star9 = images.createBigImage(`
+    . . . # . . . . . .
+    . . . . . # . . . .
+    . . # . . # . . . .
+    . . . # . . . . . .
+    . . . . . # . # . .
+    `)
+let star10 = images.createBigImage(`
+    . . . # . . . # . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . # . . . . . .
+    `)
+let star11 = images.createBigImage(`
+    . . . # . . . # . .
+    . . # . . . . . . .
+    . . . . . # . . . .
+    . . . # . . . . . .
+    # . . . . . . # . .
+    `)
+let star12 = images.createBigImage(`
+    . # . . . . . . . .
+    . . . . . . . . . .
+    . . # . # . . # . .
+    . . . . . # . . . .
+    . . . . . . # . . .
+    `)
+let star13 = images.createBigImage(`
+    . . . # . . . . . .
+    . . . . . # . . . .
+    . . # . . . . . . .
+    . . . . . . # # . .
+    . . . # # . . . . .
+    `)
+let star14 = images.createBigImage(`
+    . . . # . . # . . .
+    . . . . # . # . . .
+    . . . . . . . . . .
+    . . . . . . . # . .
+    . . . . . . . . . .
+    `)
+let star15 = images.createBigImage(`
+    . . . . # . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . # . . . . .
+    . . # . . . . # . .
+    `)
+let star16 = images.createBigImage(`
+    . . # . . . # # . .
+    . . . . . # . . . .
+    . . . # . . . . . .
+    . # . . . # . . . .
+    # . . . . . . # . .
+    `)
+let star17 = images.createBigImage(`
+    . . # . . . . . . .
+    . . . . . . . . . .
+    . . . . # . . . . .
+    . . . # . . # . . .
+    # . . . . # . . . .
+    `)
+let star18 = images.createBigImage(`
+    . . # . . . . . . .
+    . . . . . # . . . .
+    . . . . . . . . . .
+    . . # . . # . . . .
+    . . . . . # . . . .
+    `)
+let star19 = images.createBigImage(`
+    . . . . # . . . . .
+    . . . . . # . . . .
+    . # . . . . . . . .
+    . . . # # . . . . .
+    . . . . . . . . . .
+    `)
+let star20 = images.createBigImage(`
+    . . . # . . . . . .
+    . . . . # . . . . .
+    . . . . . . # . . .
+    . # . . . . . . . .
+    . . . . # . . # . #
+    `)
+let star21 = images.createBigImage(`
+    . . . . # . . . . .
+    . . # . . . . . . .
+    # . . . . . # . . .
+    . . . . . . . . . .
+    . . # . . # . . . .
+    `)
+stellas = [star1, star2, star3, star4, star5, star6, star7, star8, star9, star10, star11, star12, star13, star14, star15, star16, star17, star18, star19, star20, star21]
+for (let index = 0; index < 4; index++) {
+    current = randint(0, stellas.length - 1)
+    stellas[current].scrollImage(1, 200)
 }
-current = 0
-stellas[0].scrollImage(1, 200)
